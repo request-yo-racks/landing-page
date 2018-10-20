004f8465b50c2490118a0482b3bdc609547438b8
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-10-20 18:48:37 -0500

    Push SEO to 100

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Head from 'next/head'
+
 export default () =>
   <div>
+    <Head>
+      <title>Request Yo Racks</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="RYR landing page" />
+    </Head>
     <div className="navbar">
       <img className="logo" src="/static/images/logos/ryr_logo_64x64.png" alt="RYR logo" />
       <h1>Request Yo Racks</h1>
