cee784d7aa40d7c7a3732c164aee6a9263cc8796
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-10-20 18:09:06 -0500

    Add index page

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+export default () =>
+  <div>
+    <div className="navbar">
+      <img className="logo" src="/static/images/logos/ryr_logo_64x64.png" alt="RYR logo" />
+      <h1>Request Yo Racks</h1>
+    </div>
+    <style jsx>{`
+        h1 {
+            color: #B35C22;
+            display: inline-block;
+            margin-left: 10px;
+
+        }
+
+        .navbar > * {
+            vertical-align: middle;
+        }
+        `}
+    </style>
+  </div>
