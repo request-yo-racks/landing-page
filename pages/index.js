b9de8229c819c68cf0480b03ed4f29aba8816cad
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-10-20 19:11:35 -0500

    Link manifest.json

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ export default () =>
       <title>Request Yo Racks</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content="RYR landing page" />
+      <meta name="theme-color" content="#B35C22" />
+      <link rel="manifest" href="/static/manifest.json" />
     </Head>
     <div className="navbar">
       <img className="logo" src="/static/images/logos/ryr_logo_64x64.png" alt="RYR logo" />
