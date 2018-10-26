b919bdac1d13ad76196d1d8539a22b3e1ad5cfec
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-10-25 19:10:38 -0500

    Ensure then tests are working

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,15 +1,12 @@
 import React from 'react';
-// import { expect } from 'chai';
 import { shallow } from 'enzyme';
-// import sinon from 'sinon';
 import renderer from 'react-test-renderer'
 
-// import Index from '../pages/index';
 import App from '../pages/index.js'
 
 describe('<Index />', () => {
   it('renders <App /> components', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(App)).toExist();
+    expect(wrapper.find(App)).toBeTruthy();
   });
 });
